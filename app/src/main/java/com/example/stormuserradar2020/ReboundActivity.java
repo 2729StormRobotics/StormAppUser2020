183c7af2781a1d458e97ce88b4d66d351bc86370
irina <57159202+irinale629823@users.noreply.github.com>
2019-12-03 17:23:22 -0500

    Rebound Rumble App Page - Irina

---
+++ app/src/main/java/com/example/stormuserradar2020/ReboundActivity.java
@@ -0,0 +1,14 @@
+package com.example.stormuserradar2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReboundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rebound);
+    }
+}
