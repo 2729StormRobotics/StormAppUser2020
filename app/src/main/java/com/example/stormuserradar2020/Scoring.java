65571cd020c0e89f690fb27362d0fd24e6fe2b17
113020 <32312357+113020@users.noreply.github.com>
2019-12-10 15:56:24 -0500

    Scoring java functionality and comments

--- app/src/main/java/com/example/stormuserradar2020/Scoring.java
+++ app/src/main/java/com/example/stormuserradar2020/Scoring.java
@@ -9,21 +9,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link endgame.OnFragmentInteractionListener} interface
+ * {@link Scoring.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link endgame#newInstance} factory method to
+ * Use the {@link Scoring#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class endgame extends Fragment {
+public class Scoring extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private TextView t;
+    private static int num = 0;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -31,7 +35,7 @@ public class endgame extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public endgame() {
+    public Scoring() {
         // Required empty public constructor
     }
 
@@ -41,17 +45,18 @@ public class endgame extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment endgame.
+     * @return A new instance of fragment scoring.
      */
     // TODO: Rename and change types and number of parameters
-    public static endgame newInstance(String param1, String param2) {
-        endgame fragment = new endgame();
+    public static Scoring newInstance(String param1, String param2) {
+        Scoring fragment = new Scoring();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,8 +70,66 @@ public class endgame extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_scoring, container, false);
+
+        // *** Declare and instantiate buttons ***
+
+        // Button for adding
+        final Button scoreMore = getActivity().findViewById(R.id.btnMore);
+        scoreMore.setTag(11);
+        scoreMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increment(11);
+            }
+        });
+
+        // Button for subtracting
+        final Button scoreLess = getActivity().findViewById(R.id.btnLess);
+        scoreLess.setTag(11);
+        scoreLess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decrement(11);
+            }
+        });
+
+        // Score output Textview
+        final TextView numScored = getActivity().findViewById(R.id.txtNumScored);
+        numScored.setTag(11);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_endgame, container, false);
+        return view;
+    }
+
+    /**
+     * Increment counter using btnMore Button
+     * @param fieldID Button id
+     */
+    public void increment(int fieldID) {
+        switch (fieldID) {
+            case 11:
+                if (num < 10) {
+                    ++num;
+                    t = (TextView) getActivity().findViewById(R.id.txtNumScored);
+                    t.setText(Integer.toString(num));
+                }
+        }
+    }
+
+    /**
+     * Decrement counter using btnMore Button
+     * @param fieldID Button id
+     */
+    public void decrement(int fieldID) {
+        switch (fieldID) {
+            case 11:
+                if (num < 10) {
+                    --num;
+                    t = (TextView) getActivity().findViewById(R.id.txtNumScored);
+                    t.setText(Integer.toString(num));
+                }
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
