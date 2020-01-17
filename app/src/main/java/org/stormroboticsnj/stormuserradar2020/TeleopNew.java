9e40a3e58090cd1be3e66d0b68426e65348b2290
113020 <32312357+113020@users.noreply.github.com>
2020-01-17 14:45:05 -0500

    Update TeleopNew.java

--- app/src/main/java/org/stormroboticsnj/stormuserradar2020/TeleopNew.java
+++ app/src/main/java/org/stormroboticsnj/stormuserradar2020/TeleopNew.java
@@ -84,7 +84,7 @@ public class TeleopNew extends Fragment {
         final CheckBox rotationControl = view.findViewById(R.id.cboRC); // Checkbox for rotating control panel wheel
         final CheckBox positionControl = view.findViewById(R.id.cboPC); // Checkbox for wheel position
 
-        final MainActivity act = (MainActivity) getActivity(); // Call MainActivity object so that you can call methods from that class
+        final MainActivityNew act = (MainActivityNew) getActivity(); // Call MainActivity object so that you can call methods from that class
 
         // Setting incrementing listeners for incrementing scores to their respective buttons
         scoreMoreLVL1.setOnClickListener(new View.OnClickListener() {
